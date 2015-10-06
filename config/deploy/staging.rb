bd96d1c5c02118e9b8b5d32cd22468c2f596f25b
Ben Robertson <benjamingrobertson@me.com>
2015-10-06 14:14:51 -0400

    Update staging file

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,12 +2,12 @@
 # This deploy recipe will deploy a project from a github repo to a VPS server
 #
 # Assumptions:
-#   * You are using VPS hosting with SSH access
-#   * Your deployment directory is located in /srv/www/
+# * You are using VPS hosting with SSH access
+# * Your deployment directory is located in /srv/www/
 #
 #################################################################################################################
 #
-# Change this to the name of the project.  It should match the name of the Git repo.
+# Change this to the name of the project. It should match the name of the Git repo.
 # This will set the name of the project directory and become the subdomain
 set :project, 'betsch' # the GitHub project name
 set :github_user, "mergeweb" # Your GitHub username
@@ -18,12 +18,12 @@ set :domain, 'luigi.mergeweb.net' # IP or domain name to ssh to
 #### You shouldn't need to change anything below ########################################################
 default_run_options[:pty] = true
 
-set :repository,  "git@github.com:#{github_user}/#{project}.git" #GitHub clone URL
+set :repository, "git@github.com:#{github_user}/#{project}.git" #GitHub clone URL
 set :scm, "git"
 set :scm_passphrase, "" # This is the passphrase for the ssh key on the server deployed to
-set :branch, "master"   # This is for a git branch if you want to deploy from something besides master
+set :branch, $1 if `git branch` =~ /\* (\S+)\s/m # This is for a git branch if you want to deploy from something besides master
 set :scm_verbose, true
-set :applicationdir, "/var/www/vhosts/mergeweb.net/httpdocs/#{domain_name}"
+set :applicationdir, "/var/www/websites/#{domain_name}"
 set :keep_releases, 3
 
 # Don't change this stuff, but you may want to set shared files at the end of the file ##################
@@ -34,7 +34,7 @@ set :deploy_via, :remote_cache
 # roles (servers)
 role :app, domain
 role :web, domain
-role :db,  domain, :primary => true
+role :db, domain, :primary => true
 
 namespace :deploy do
   desc "Restarting mod_rails with restart.txt"
@@ -55,6 +55,5 @@ set :use_sudo, false
 after "deploy:update_code" do
     run "ln -s #{shared_path}/sites/default #{release_path}/sites"
     run "ln -s #{shared_path}/cache #{release_path}"
-    run "(cd #{release_path}/sites/all/themes/merge/scss/; compass compile -c config.rb merge.style.scss)"
 end
 after "deploy", "deploy:cleanup"
